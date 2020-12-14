8e688467ac97051b6d6db001dcd29244bfe2068a
omgpiu <dyba@list.ru>
2020-12-14 16:00:34 +0300

    initial commit

---
+++ src/filter/123.js
@@ -0,0 +1,18 @@
+function sum(numbers) {
+    if (numbers.lenght > 1) {
+        const reducer = (acc, val) => acc + val
+        return numbers.reduce(reducer)
+    }else
+    if (numbers.lenght === 1) {
+        return numbers[0]
+
+    } else {
+        return 0
+    }
+
+
+}
+
+
+const ar = [1, 2, 3, 4, 5]
+console.log(sum(ar))
