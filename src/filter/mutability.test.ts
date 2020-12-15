3e9209bc1429cd623f5b903351ed12f3061d1d39
omgpiu <dyba@list.ru>
2020-12-15 14:58:49 +0300

    initial commit

--- src/filter/mutability.test.ts
+++ src/filter/mutability.test.ts
@@ -1,4 +1,22 @@
-import {userType} from "./mutability";
+import {changeStudents, moveUser, mvUser, userType} from "./mutability";
+
+let user: userType;
+beforeEach(() => {
+    user = {
+        name: 'sasha',
+        age: 10,
+        address: {
+            city: 'Moscow'
+        },
+        school: {
+            address: 'street',
+            houseNumber: 10,
+            students: 1000
+
+        }
+    }
+})
+
 
 export const increaseAge = (user: userType) => {
     return {
@@ -9,14 +27,22 @@ export const increaseAge = (user: userType) => {
 
 
 test('increase age', () => {
-    let user: userType = {
-        name: 'sasha',
-        age: 10,
-        address: {
-            city: 'Moscow'
-        }
-    }
+
     const copyUser = increaseAge(user)
     expect(user.age).toBe(10)
     expect(copyUser.age).toBe(20)
+})
+test('change city', () => {
+
+    const newCity = moveUser(user, 'Chita')
+    const nwCity = mvUser(user, 'Saint')
+    expect(newCity.address.city).toBe('Chita')
+    expect(nwCity.address.city).toBe('Saint')
+
+})
+test('change school students', () => {
+
+    const changedAmountOfStudents = changeStudents(user, 500)
+    expect(changedAmountOfStudents.school.students).toBe(500)
+
 })
--- src/filter/mutability.ts
+++ src/filter/mutability.ts
@@ -1,8 +1,58 @@
+import userEvent from "@testing-library/user-event";
+
 export type userType = {
     name: string
     age: number
-
-    address?: {
+    address: {
         city: string
+    }
+    school: SchoolType
+}
+export type SchoolType = {
+    address: string
+    houseNumber: number
+    students: number
+}
+
+export const moveUser = (user: userType, city: string) => {
+    const copy = {
+        ...user,
+    }
+
+    copy.address = {
+        ...copy, city: city
+    }
+    return copy
+}
+// export const mvUr = (user: userType, city: string) => {
+//     return {
+//         ...user,
+//         address: {
+//             ...user.address,
+//             city: city
+//         }
+//
+//     }
+// }
+
+export const changeStudents = (user: userType, students: number) => {
+
+    return {
+        ...user,
+        school: {
+            ...user.school,
+            students: students
+        }
+    }
+}
+
+
+export const mvUser = (user:userType,city:string)=>{
+    return {
+        ...user,
+        address:{
+            ...user.address,
+            city:city
+        }
     }
 }
